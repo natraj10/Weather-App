4cf107ea6410c4d8b3b6b2858d5a941e826ed241
Natraj10 <natrajprabu1988@gmail.com>
2018-12-16 20:46:21 +0000

    final_2 version

--- app.py
+++ app.py
@@ -19,9 +19,10 @@ class City(db.Model):
 def index():
     if request.method=='POST':
         new_city=request.form.get('city')
-        new_city_obj=City(name=new_city)
-        db.session.add(new_city_obj)
-        db.session.commit()
+        if new_city:
+            new_city_obj=City(name=new_city)
+            db.session.add(new_city_obj)
+            db.session.commit()
 
     c=City.query.all()
     url='http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&APPID=6cdf12445c7e9aeb333fd66c9ad6cc21'
